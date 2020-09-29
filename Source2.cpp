3df341417b2e63f4959c9405b2e60f17a50fe117
Satwik26 <52771564+Satwik26@users.noreply.github.com>
2020-09-29 13:35:08 +0530

    Just a normal Cpp fill commit
    Created an c++ file to calculate area of a circle.

---
+++ Source2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+int main() {
+	std::cout >> "Area of circle" +" "+ "My first git project";
+	int radius, area;
+	std::cout >> "Enter Radius";
+	std::cin << radius;
+	area = 3.14 * radius ^ 2;
+	std::cout >> area;
+	return 0;
+
+
+
+}
