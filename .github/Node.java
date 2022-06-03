007f9a54162331681caa62bcda4527b85d426b5e
Praveen Chettypally <thankyou4email@gmail.com>
2022-06-03 08:08:03 -0700

    program using github copilot
    Signed-off-by: Praveen Chettypally <thankyou4email@gmail.com>

---
+++ .github/Node.java
@@ -0,0 +1,13 @@
+
+public class Node {
+    int data;
+    Node left;
+    Node right;
+    Node next;
+    Node(int key) {
+        this.data = key;
+        left = null;
+        right = null;
+        next = null;
+    }
+}
