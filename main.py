e4a77910a4cf5b404c907ce301b1e5ecc106eece
T-Wesst <tyroojwest@gmail.com>
2022-06-05 00:13:00 -0700

    visit each url with chrome webdriver

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
+from selenium import webdriver
 import re
+path = "./chromedriver"
+driver = webdriver.Chrome(path)
 with open('README.md', 'r') as readFile:
-  with open('urls.txt', 'w') as writeFile:
-    strUrls = '\(http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*(),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
-    urls = re.findall(strUrls, readFile.read())
-    for url in urls:
-      link = url.replace("(","").replace(")","")
-      regex = re.compile('http[s]?:\/\/(?:github|gitlab).com')
-      if regex.search(link):
-        writeFile.write(f'{link}\n')
-      # go to each url
-      # if the web page contains the message: "No results matched your search"
-      # annoted the current url with "outdated" || checkbox or create an automated issue/notification to main contributor
+    with open('urls.txt', 'w') as writeFile:
+        strUrls = '\(http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*(),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+        urls = re.findall(strUrls, readFile.read())
+        for url in urls:
+            link = url.replace("(", "").replace(")", "")
+            regex = re.compile('http[s]?:\/\/(?:github|gitlab).com')
+            if regex.search(link):
+                writeFile.write(f'{link}\n')
+                # go to each url
+                driver.get(link)
+                # if the web page contains the message: "No results matched your search"
+                # annoted the current url with "outdated" || checkbox or create an automated issue/notification to main contributor
