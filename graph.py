9aa887ca72482fbd4fde1137e9a4607f09d7df1e
Prathamesh Walawalkar <51331118+RenmoTC@users.noreply.github.com>
2022-10-28 23:11:29 +0530

    Create graph.py

---
+++ graph.py
@@ -0,0 +1,21 @@
+
+# importing the required module
+import matplotlib.pyplot as plt
+  
+# x axis values
+x = [1,2,3]
+# corresponding y axis values
+y = [2,4,1]
+  
+# plotting the points 
+plt.plot(x, y)
+  
+plt.xlabel('x - axis')
+# naming the y axis
+plt.ylabel('y - axis')
+  
+# giving a title to my graph
+plt.title('My first graph!')
+  
+# function to show the plot
+plt.show()
