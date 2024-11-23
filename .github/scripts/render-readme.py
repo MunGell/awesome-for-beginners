377118e21ec655735da8615c12aecd79b8a364ba
Sammy Hori <git@sammyhori.com>
2024-11-23 09:49:46 +0000

    ruff formatting

--- .github/scripts/render-readme.py
+++ .github/scripts/render-readme.py
@@ -6,16 +6,21 @@ TEMPLATEPATH = "./.github/"
 TEMPLATEFILE = "README-template.j2"
 TARGETFILE = "./README.md"
 
+
 def new_technology_dict(repo_technology):
     return {"link_id": repo_technology.lower(), "entries": []}
 
+
 technologies = {}
 
-with open(DATAFILE, 'r') as datafile:
+with open(DATAFILE, "r") as datafile:
     data = json.loads(datafile.read())
 
 for technology in data["technologies"]:
-    technologies[technology] = {"link_id": data["technologies"][technology], "entries": []}
+    technologies[technology] = {
+        "link_id": data["technologies"][technology],
+        "entries": [],
+    }
 
 for repository in data["repositories"]:
     repo_technologies = repository["technologies"]
@@ -24,12 +29,14 @@ for repository in data["repositories"]:
             technologies[repo_technology] = new_technology_dict(repo_technology)
         technologies[repo_technology]["entries"].append(repository)
 
-env = Environment(loader = FileSystemLoader(TEMPLATEPATH))
+env = Environment(loader=FileSystemLoader(TEMPLATEPATH))
 template = env.get_template(TEMPLATEFILE)
 
 categories = []
 for key, value in zip(technologies.keys(), technologies.values()):
-    categories.append({"title": key, "link_id": value["link_id"], "entries": value["entries"]})
+    categories.append(
+        {"title": key, "link_id": value["link_id"], "entries": value["entries"]}
+    )
 
 categories = sorted(categories, key=lambda x: x["title"].upper())
 for category in categories:
